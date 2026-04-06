62dc906071b6988d3b1043fbba099b99324aad1c
NeroTouzjian <nerotouzjian10@gmail.com>
2026-04-06 04:16:02 +0300

    message

--- src/main/java/projects/workshop1project/Models/EmployeeRecord.java
+++ src/main/java/projects/workshop1project/Models/EmployeeRecord.java
@@ -1,4 +1,29 @@
 package projects.workshop1project.Models;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.time.LocalDate;
+
 public class EmployeeRecord {
+    private SimpleStringProperty empName;
+    private SimpleStringProperty empId;
+    private SimpleStringProperty empAge;
+    private SimpleStringProperty departmentName;
+    private SimpleStringProperty empPosition;
+    private SimpleIntegerProperty salary;
+    private SimpleObjectProperty<LocalDate> hireDate;
+
+    public EmployeeRecord(String empName , String empId , String empAge , String departmentName , String empPosition , int salary , LocalDate hireDate){
+        this.empName = new SimpleStringProperty(empName);
+        this.empId  = new SimpleStringProperty(empId);
+        this.empAge = new SimpleStringProperty(empAge);
+        this.departmentName = new SimpleStringProperty(departmentName);
+        this.empPosition = new SimpleStringProperty(empPosition);
+        this.salary = new SimpleIntegerProperty(salary);
+        this.hireDate = new SimpleObjectProperty<LocalDate>(hireDate);
+
+    }
+
 }
